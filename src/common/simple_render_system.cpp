f90ecf07c425098cb0130b97ff089719e1d80a17
Arsha <arsha.shiri@gmail.com>
2024-05-18 08:33:01 +0200

    feat: add view to the camera class (vid 14)

--- src/common/simple_render_system.cpp
+++ src/common/simple_render_system.cpp
@@ -30,6 +30,8 @@ void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer,
 {
     pipeline_->bind(commandBuffer);
 
+    const auto projectionView = camera.getProjection() * camera.getView();
+
     for (auto &obj : gameObjects)
     {
         obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
@@ -37,7 +39,7 @@ void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer,
 
         SimplePushConstantData push{};
         push.color = obj.color;
-        push.transform = camera.getProjection() * obj.transform.mat4();
+        push.transform = projectionView * obj.transform.mat4();
 
         vkCmdPushConstants(commandBuffer,
                            pipelineLayout_,
