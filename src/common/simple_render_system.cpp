e3bfabc96e20afe387ef180f4a1315984743b470
Arsha <arsha.shiri@gmail.com>
2024-06-08 14:41:05 +0200

    feat: demonstrate diffuse lighting (vid 18)

--- src/common/simple_render_system.cpp
+++ src/common/simple_render_system.cpp
@@ -14,7 +14,7 @@ namespace
 struct SimplePushConstantData
 {
     glm::mat4 transform{1.f};
-    alignas(16) glm::vec3 color;
+    glm::mat4 normalMatrix{1.f};
 };
 } // namespace
 
@@ -35,8 +35,9 @@ void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer,
     for (auto &obj : gameObjects)
     {
         SimplePushConstantData push{};
-        push.color = obj.color;
-        push.transform = projectionView * obj.transform.mat4();
+        auto modelMatrix = obj.transform.mat4();
+        push.transform = projectionView * modelMatrix;
+        push.normalMatrix = obj.transform.normalMatrix();
 
         vkCmdPushConstants(commandBuffer,
                            pipelineLayout_,
